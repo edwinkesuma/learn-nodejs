4d1ee875d55fb86165d0ee7e1a5f45684d28074e
edwinkesuma <53287515+edwinkesuma@users.noreply.github.com>
2025-06-27 22:18:28 +0700

    build a very simple API

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ const url = require("url");
 // console.log("Will read file");
 
 // ========================================================= Server related
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -36,6 +39,11 @@ const server = http.createServer((req, res) => {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/produtc") {
     res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
